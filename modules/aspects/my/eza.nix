e4784708cef439cce35bd8153c66645e691d9395
1Step621 <onestep621@gmail.com>
2026-07-03 14:21:33 +0900

    eza: add tree abbr

--- modules/aspects/my/eza.nix
+++ modules/aspects/my/eza.nix
@@ -12,6 +12,11 @@
         la = "eza -a";
         ll = "eza -l";
         lla = "eza -la";
+        lt = "eza --tree";
+        lt2 = "eza --tree --level=2";
+        lt3 = "eza --tree --level=3";
+        lt4 = "eza --tree --level=4";
+        lt5 = "eza --tree --level=5";
       };
     };
   };
